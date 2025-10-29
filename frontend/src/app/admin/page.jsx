2c7aa5eb6a9b69aa9b669337bef4744205732568
Joseph Ruano <git@josephinfra.dev>
2025-10-29 15:06:54 -0600

    fix: formulario de materias

--- frontend/src/app/admin/page.jsx
+++ frontend/src/app/admin/page.jsx
@@ -1,5 +1,6 @@
 'use client';
 import { useState, useEffect } from 'react';
+import { useRouter } from "next/navigation";
 import AlumnoForm from './components/AlumnoForm';
 import DocenteForm from './components/DocenteForm';
 import AdminForm from './components/AdminForm';
@@ -10,6 +11,8 @@ import TablaAdmin from './components/TablaAdmin';
 const BACK_URL = process.env.NEXT_PUBLIC_BACK_URL;
 
 export default function AdminPage() {
+    const router = useRouter();
+
   const [registros, setRegistros] = useState([]);
   const [formData, setFormData] = useState({});
   const [error, setError] = useState('');
@@ -225,6 +228,12 @@ const guardarRegistro = async (e) => {
     <main className="min-h-screen p-6 bg-gray-100 dark:bg-gray-900 text-gray-900 dark:text-gray-100">
       <div className="max-w-6xl mx-auto flex flex-col gap-6">
         <h1 className="text-4xl font-bold text-center mb-4">Panel de Administración</h1>
+          <button
+            onClick={() => router.push("/perfil")}
+            className="w-full p-2 bg-blue-500 hover:bg-blue-600 text-white rounded transition"
+          >
+            ← Regresar al perfil
+          </button>
 
         {/* Formulario */}
         <div className="bg-white dark:bg-gray-800 shadow rounded p-6 flex flex-col gap-4">
